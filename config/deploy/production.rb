e91f05d625d22e9ba63333ecf10b111cbf03f4a0
shumylosasha <shumylo.s@gmail.com>
2021-07-14 23:23:51 +0300

    new ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
-server '18.118.186.69', user: 'deploy', roles: %w{web app db}
+server '18.118.227.249', user: 'deploy', roles: %w{web app db}
 set :rails_env, 'production'
